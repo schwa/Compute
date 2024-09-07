8c7a31d37685a2cabf72a99c66699bd53364f1e5
Jonathan Wight <jwight@mac.com>
2024-09-07 08:18:50 -0700

    WIP

--- Sources/Examples/Examples/CountingSort/CountingSort.swift
+++ Sources/Examples/Examples/CountingSort/CountingSort.swift
@@ -20,28 +20,26 @@ struct CountingSortDemo {
         shufflePipeline = try compute.makePipeline(function: library.function(name: "CountingSort::shuffle1"))
     }
 
-    func radixSort(dispatch: Compute.Dispatcher, input: TypedMTLBuffer<UInt32>) throws -> TypedMTLBuffer<UInt32> {
+    func radixSort(input: TypedMTLBuffer<UInt32>) throws -> TypedMTLBuffer<UInt32> {
         var input = input
         let device = compute.device
         var output: TypedMTLBuffer<UInt32> = try device.makeTypedBuffer(count: input.count)
         for phase in 0..<3 {
             let shift = phase * 8
-            print("PHASE")
-            try countingSort(dispatch: dispatch, input: input, output: output, shift: shift)
+            try countingSort(input: input, output: output, shift: shift)
             swap(&input, &output)
         }
-        return output
+        return input
     }
 
-    func countingSort1(dispatch: Compute.Dispatcher, input: TypedMTLBuffer<UInt32>) throws -> TypedMTLBuffer<UInt32> {
+    func countingSort1(input: TypedMTLBuffer<UInt32>) throws -> TypedMTLBuffer<UInt32> {
         let device = compute.device
         let output: TypedMTLBuffer<UInt32> = try device.makeTypedBuffer(count: input.count)
-        try countingSort(dispatch: dispatch, input: input, output: output, shift: 0)
+        try countingSort(input: input, output: output, shift: 0)
         return output
     }
 
-
-    func countingSort(dispatch: Compute.Dispatcher, input: TypedMTLBuffer<UInt32>, output: TypedMTLBuffer<UInt32>, shift: Int) throws {
+    func countingSort(input: TypedMTLBuffer<UInt32>, output: TypedMTLBuffer<UInt32>, shift: Int) throws {
         var histogramPipeline = histogramPipeline
         var prefixSumPipeline = prefixSumPipeline
         var shufflePipeline = shufflePipeline
@@ -53,23 +51,28 @@ struct CountingSortDemo {
         histogramPipeline.arguments.count = .int(input.count)
         histogramPipeline.arguments.shift = .int(shift)
 
-        try dispatch(pipeline: histogramPipeline, threads: MTLSize(width: input.count), threadsPerThreadgroup: histogramPipeline.calculateThreadgroupSize(threads: MTLSize(width: input.count)))
+        try compute.run(pipeline: histogramPipeline, threads: MTLSize(width: input.count), threadsPerThreadgroup: histogramPipeline.calculateThreadgroupSize(threads: MTLSize(width: input.count)))
+
+        assert(RadixSortCPU().histogram(input: Array(input), shift: shift) == Array(histogram))
+
 
         let summedHistogram: TypedMTLBuffer<UInt32> = try device.makeTypedBuffer(count: 256)
         prefixSumPipeline.arguments.inputs = .buffer(histogram)
         prefixSumPipeline.arguments.outputs = .buffer(summedHistogram)
         prefixSumPipeline.arguments.count = .int(histogram.count)
 
-        try dispatch(pipeline: prefixSumPipeline, threads: MTLSize(width: 256), threadsPerThreadgroup: prefixSumPipeline.calculateThreadgroupSize(threads: MTLSize(width: 256)))
+        try compute.run(pipeline: prefixSumPipeline, threads: MTLSize(width: 256), threadsPerThreadgroup: prefixSumPipeline.calculateThreadgroupSize(threads: MTLSize(width: 256)))
 
-        assert(Array(summedHistogram) == Array(histogram).prefixSumInclusive())
+        assert(RadixSortCPU().prefixSumExclusive(RadixSortCPU().histogram(input: Array(input), shift: shift)) == Array(summedHistogram))
+
 
         shufflePipeline.arguments.input = .buffer(input)
         shufflePipeline.arguments.count = .int(input.count)
         shufflePipeline.arguments.output = .buffer(output)
         shufflePipeline.arguments.shift = .int(shift)
         shufflePipeline.arguments.histogram = .buffer(summedHistogram)
-        try dispatch(pipeline: shufflePipeline, threads: MTLSize(width: input.count), threadsPerThreadgroup: shufflePipeline.calculateThreadgroupSize(threads: MTLSize(width: input.count)))
+        try compute.run(pipeline: shufflePipeline, threads: MTLSize(width: input.count), threadsPerThreadgroup: shufflePipeline.calculateThreadgroupSize(threads: MTLSize(width: input.count)))
+
     }
 }
 
@@ -79,20 +82,21 @@ extension CountingSortDemo: Demo {
         let device = MTLCreateSystemDefaultDevice()!
         let compute = try Compute(device: device, logger: Logger(), logging: logging)
         let count = 10
-//        let elements: [UInt32] = (0..<count).map { _ in UInt32.random(in: 0..<100) }
-        let elements: [UInt32] = (0..<count).map { UInt32(count - $0 - 1) }
+        let elements: [UInt32] = (0..<count).map { _ in UInt32.random(in: 0..<10) }
+//        let elements: [UInt32] = (0..<count).map { UInt32(count - $0 - 1) }
         let input = try device.makeTypedBuffer(data: elements)
         let sort = try CountingSortDemo(compute: compute)
 
         let output = try device.capture(enabled: Self.capture) {
             try compute.task { task in
                 try task { dispatch in
-                    try sort.countingSort1(dispatch: dispatch, input: input)
+                    try sort.countingSort1(input: input)
                 }
             }
         }
 
-        print(Array(output))
+        print(RadixSortCPU().radixSort(input: elements) == elements.sorted())
         print(Array(output) == elements.sorted())
+        print(Array(output))
     }
 }
--- Sources/Examples/Examples/CountingSort/RadixSortCPU.swift
+++ Sources/Examples/Examples/CountingSort/RadixSortCPU.swift
@@ -2,8 +2,8 @@ import Foundation
 
 postfix operator ++
 
-extension Int {
-    static postfix func ++(rhs: inout Int) -> Int {
+extension BinaryInteger {
+    static postfix func ++(rhs: inout Self) -> Self {
         let oldValue = rhs
         rhs += 1
         return oldValue
@@ -11,31 +11,44 @@ extension Int {
 }
 
 // From: "A High-Performance Implementation of Counting Sort on CUDA GPU"
-func countingSort(input: [UInt32], shift: Int, output: inout [UInt32]) {
-    func key(_ value: UInt32) -> Int {
+
+struct RadixSortCPU {
+
+    func key(_ value: UInt32, shift: Int) -> Int {
         (Int(value) >> shift) & 0xFF
     }
-    // Histogram
-    var histogram = input.reduce(into: Array(repeating: 0, count: 256)) { result, value in
-        result[key(value)] += 1
+
+    func histogram(input: [UInt32], shift: Int) -> [UInt32] {
+        input.reduce(into: Array(repeating: 0, count: 256)) { result, value in
+            result[key(value, shift: shift)] += 1
+        }
     }
 
-    histogram = histogram.prefixSumExclusive()
-
-    // Shuffle
-    for i in input.indices {
-        let value = key(input[i])
-        output[histogram[value]++] = input[i]
+    func prefixSumExclusive(_ input: [UInt32]) -> [UInt32] {
+        input.prefixSumExclusive()
     }
-}
 
-func radixSort(values: [UInt32]) -> [UInt32] {
-    var input = values
-    var output = Array(repeating: UInt32.zero, count: input.count)
-    for phase in 0..<4 {
-        countingSort(input: input, shift: phase * 8, output: &output)
-        swap(&input, &output)
-        //        print("Phase: \(phase) \(input.map({ String($0, radix: 16) }))")
+    func shuffle(_ input: [UInt32], summedHistogram histogram: [UInt32], shift: Int, output: inout [UInt32]) {
+        var histogram = histogram
+        for i in input.indices {
+            let value = key(input[i], shift: shift)
+            output[Int(histogram[value]++)] = input[i]
+        }
     }
-    return input
+
+    func countingSort(input: [UInt32], shift: Int, output: inout [UInt32]) {
+        let histogram = histogram(input: input, shift: shift)
+        let summedHistogram = prefixSumExclusive(histogram)
+        shuffle(input, summedHistogram: summedHistogram, shift: shift, output: &output)
+    }
+
+    func radixSort(input: [UInt32]) -> [UInt32] {
+        var input = input
+        var output = Array(repeating: UInt32.zero, count: input.count)
+        for phase in 0..<4 {
+            countingSort(input: input, shift: phase * 8, output: &output)
+            swap(&input, &output)
+        }
+        return input
+    }
 }
